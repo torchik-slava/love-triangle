1d03c61f3da157e00299b7e0564c23be9b8fe60d
Slava Torchik <torhik_slava@mail.ru>
2018-09-12 21:49:52 +0300

    love-triangle/solution: second task completed

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var n = 0;
+  for (var i=0; i<preferences.length; i++){
+    var j = preferences[i]-1;
+    var k = preferences[j]-1;
+    if(preferences[i]===j+1 && preferences[j]===k+1 && preferences[k]===i+1 &&
+      preferences[i]!==preferences[j] && preferences[i]!==preferences[k]){
+      n++;
+    }
+  }
+  return n/3;
+
 };
